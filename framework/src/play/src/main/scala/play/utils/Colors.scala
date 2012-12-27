7f607033320a97a3f896743a8d58db06701e4b3b
Valerian Barbot <vbr@zenexity.com>
2012-12-27 21:55:50 +0100

    refactoring Colors

--- framework/src/play/src/main/scala/play/utils/Colors.scala
+++ framework/src/play/src/main/scala/play/utils/Colors.scala
@@ -7,19 +7,26 @@ object Colors {
   lazy val isANSISupported = {
     Option(System.getProperty("sbt.log.noformat")).map(_ != "true").orElse {
       Option(System.getProperty("os.name"))
-        .map(_.toLowerCase)
-        .filter(_.contains("windows"))
-        .map(_ => false)
+        .map(!_.toLowerCase.contains("windows"))
     }.getOrElse(true)
   }
 
-  def red(str: String): String = if (isANSISupported) (RED + str + RESET) else str
-  def blue(str: String): String = if (isANSISupported) (BLUE + str + RESET) else str
-  def cyan(str: String): String = if (isANSISupported) (CYAN + str + RESET) else str
-  def green(str: String): String = if (isANSISupported) (GREEN + str + RESET) else str
-  def magenta(str: String): String = if (isANSISupported) (MAGENTA + str + RESET) else str
-  def white(str: String): String = if (isANSISupported) (WHITE + str + RESET) else str
-  def black(str: String): String = if (isANSISupported) (BLACK + str + RESET) else str
-  def yellow(str: String): String = if (isANSISupported) (YELLOW + str + RESET) else str
+  def wrap(colorCode: String)(str: String): String = if (isANSISupported) (colorCode + str + RESET) else str
+
+  val red: String => String = wrap(RED)
+
+  val blue: String => String = wrap(BLUE)
+
+  val cyan: String => String = wrap(CYAN)
+
+  val green: String => String = wrap(GREEN)
+
+  val magenta: String => String = wrap(MAGENTA)
+
+  val white: String => String = wrap(WHITE)
+
+  val black: String => String = wrap(BLACK)
+
+  val yellow: String => String = wrap(YELLOW)
 
 }
